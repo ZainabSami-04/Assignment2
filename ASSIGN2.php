2ccbd4c8286513979e10e2561a218fc0d60ec08c
ZainabSami-04 <zai04nab@gmail.com>
2024-11-28 10:13:09 +0300

    comments

--- ASSIGN2.php
+++ ASSIGN2.php
@@ -1,5 +1,9 @@
 <?php
+//Task 1:Data Retrival 
+
+//Link to fetch data from the Bahrain Open Data Portal API
 $url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
+
 
 $response = file_get_contents($url);
 
@@ -7,25 +11,30 @@ if ($response === FALSE) {
     die("Error: Unable to fetch API data.");
 }
 
+//JSON Decode
 $data = json_decode($response, true);
 
+//If condition if there is no data availablr to fetch
 if (!$data || !isset($data['results'])) {
     die("No data available");
 }
+
+//Task 2: Data Visualization
 
 echo "<!DOCTYPE html>";
 echo "<html lang='en'>";
 echo "<head>
         <meta charset='UTF-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <title>Table</title>
-        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css'>
-    
+        <title>Assignment 2</title>
+       <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css'>
+       
       </head>";
 echo "<body>";
 echo "<main class='container'>";
 echo "<h1>Comprehensive View of Student Demographics at the University of Bahrain</h1>";
 
+//HTML table to display the retreived data
 echo "<table>";
 echo "<thead data-theme='light'>
         <tr>
